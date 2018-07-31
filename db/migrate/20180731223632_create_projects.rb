05a324b799155246d7889c8e2af81b6f0d34c64f
Itspacome <pacome.perrichot@gmail.com>
2018-08-01 01:17:20 +0200

    destroy g model Project & g scaffold Project

--- db/migrate/20180731223632_create_projects.rb
+++ db/migrate/20180731223632_create_projects.rb
@@ -5,10 +5,8 @@ class CreateProjects < ActiveRecord::Migration[5.2]
       t.string :short_description
       t.text :description
       t.text :why_join
-      t.boolean :approved, null: true, default: false
-      t.belongs_to :user, foreign_key: true
+
       t.timestamps
     end
-      add_index :projects, :title, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_221552) do
+ActiveRecord::Schema.define(version: 2018_07_31_223632) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
